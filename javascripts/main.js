8efa1f41b991249c3c9de843ef1353efc41bfc1b
Mitchell Blom <mitchellblom@gmail.com>
2017-04-21 12:05:48 -0500

    loading hardcoded user json to console

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
 console.log("main js works");
+
+let leftUserJSON;
+
+        const loadLeftUserJSON = function() {
+            return new Promise((resolve, reject) => {
+                $.ajax("https://teamtreehouse.com/geoffwebb.json").done(function(data1) {
+                    resolve(data1);										// was resolve(data1.products);
+                }).fail((error1) => {
+                    reject(error1);
+                    console.log("error1", error1);
+                });
+            });
+        };
+
+        loadLeftUserJSON().then((results) => {
+            leftUserJSON = results;
+            console.log(leftUserJSON);
+        });
+
+
+        //writeUserToDom function here
