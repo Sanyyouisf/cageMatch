5dff378b79d3b62f71f9655d594df9a78bdd27ce
Mitchell Blom <mitchellblom@gmail.com>
2017-04-21 13:18:42 -0500

    buttons linked, setting up for right user load to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,35 +1,63 @@
+$(document).ready(() => {
+
+	let leftUserJSON;
+	let rightUserJSON;
+
+	let leftUserObject = $("#left-user").val();
+	let leftUserURl = `https://teamtreehouse.com/${leftUserObject}.json`;
+
+	let rightUserObject = $("#right-user").val();
+	let rightUserURl = `https://teamtreehouse.com/${rightUserObject}.json`;
 
 
-let leftUserJSON;
-let leftUser = $("#left-user").val();
+	$("#left-user-button").click(function() {
+		writeLeftUserToDom(leftUserJSON);
+	})
 
-let leftUserURl = `https://teamtreehouse.com/${leftUser}.json`;
+	$("#right-user-button").click(function() {
+		writeRightUserToDom(rightUserJSON);
+	})
 
-        const loadLeftUserJSON = function() {
-            return new Promise((resolve, reject) => {
-                $.ajax(leftUserURl).done(function(data1) {
-                    resolve(data1);										
-                }).fail((error1) => {
-                    reject(error1);
-                    console.log("error1", error1);
-                });
-            });
-        };
 
-        loadLeftUserJSON().then((results) => {
-            leftUserJSON = results;
-            console.log(leftUserJSON);
-            writeLeftUserToDom(leftUserJSON)
-        });
+	const loadLeftUserJSON = () => {
+	    return new Promise((resolve, reject) => {
+	        $.ajax(leftUserURl).done(function(data1) {
+	            resolve(data1);
+	        }).fail((error1) => {
+	            reject(error1);
+	            console.log("error1", error1);
+	        });
+	    });
+	};
 
-        const writeLeftUserToDom = (leftUserJSON) => {
-        	let leftUserString = "";
-        	leftUserString += `<div class="user-container">`;
-        	leftUserString += `<div>Fighter One:</div>`;
-        	leftUserString += `<div id="left-user-on-dom">${leftUserJSON.name}</div>`;
-        	leftUserString += `<div>Total Points:</div>`;
-        	leftUserString += `<div id="leftUserPoints">${leftUserJSON.points.total}</div>`;
-        	leftUserString += `<div class="img-cont"><img class="user-image" src="${leftUserJSON.gravatar_url}" alt="left user image"></div>`;
-        	leftUserString += `</div>`
-        	$("#userContainerLeft").append(leftUserString);
-        }
+	loadLeftUserJSON().then((results) => {
+	        leftUserJSON = results;
+	        console.log(leftUserJSON);
+	        // writeLeftUserToDom(userJSON)
+	    });
+
+	const writeLeftUserToDom = (leftUserJSON) => {
+	    let userString = "";
+	    userString += `<div class="user-container">`;
+	    userString += `<div>Fighter One:</div>`;
+	    userString += `<div id="left-user-on-dom">${leftUserJSON.name}</div>`;
+	    userString += `<div>Total Points:</div>`;
+	    userString += `<div id="leftUserPoints">${leftUserJSON.points.total}</div>`;
+	    userString += `<div class="img-cont"><img class="user-image" src="${leftUserJSON.gravatar_url}" alt="left user image"></div>`;
+	    userString += `</div>`
+	    $("#userContainerLeft").append(userString);
+	}
+
+	const writeRightUserToDom = (rightUserJSON) => {
+	    let userString = "";
+	    userString += `<div class="user-container">`;
+	    userString += `<div>Fighter One:</div>`;
+	    userString += `<div id="right-user-on-dom">${rightUserJSON.name}</div>`;
+	    userString += `<div>Total Points:</div>`;
+	    userString += `<div id="rightUserPoints">${rightUserJSON.points.total}</div>`;
+	    userString += `<div class="img-cont"><img class="user-image" src="${rightUserJSON.gravatar_url}" alt="right user image"></div>`;
+	    userString += `</div>`
+	    $("#userContainerRight").append(userString);
+	}
+
+});
